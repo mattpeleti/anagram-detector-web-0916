ebb676722e48530a1fb2dc59f12e902b445837fa
github username <github email address>
2016-09-29 17:12:48 -0400

    Chris is a god

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+# class Anagram
+#   attr_reader :match
+#   def initialize(word)
+#     @word = word
+#   end
+#
+#   def match(poss_ana_array)
+#
+#     poss_ana_array.select { |poss_ana| @word.chars.select {|char| poss_ana.include?(char)}  }
+#
+#   end
+#
+#
+# end
+
+
+class Anagram
+  attr_reader :match
+  def initialize(word)
+    @word = word
+  end
+
+
+  def match(poss_ana_array)
+    poss_ana_array.select { |poss_ana| @word.chars.sort == poss_ana.chars.sort }
+  end
+end
